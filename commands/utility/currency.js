9d81750edc1d504519f4f4e894ccb224110458a5
dwicao <lutfiandwica@gmail.com>
2019-08-04 08:27:33 +0700

    Feat: add Currency Converter cmd

---
+++ commands/utility/currency.js
@@ -0,0 +1,96 @@
+const { RichEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const { prefix } = require("../../config");
+
+const RATES = [
+  "CAD",
+  "HKD",
+  "ISK",
+  "PHP",
+  "DKK",
+  "HUF",
+  "CZK",
+  "GBP",
+  "RON",
+  "SEK",
+  "IDR",
+  "INR",
+  "BRL",
+  "RUB",
+  "HRK",
+  "JPY",
+  "THB",
+  "CHF",
+  "EUR",
+  "MYR",
+  "BGN",
+  "TRY",
+  "CNY",
+  "NOK",
+  "NZD",
+  "ZAR",
+  "USD",
+  "MXN",
+  "SGD",
+  "AUD",
+  "ILS",
+  "KRW",
+  "PLN"
+];
+
+module.exports = {
+  name: "currency",
+  description: "Foreign exchange rates",
+  aliases: ["cur"],
+  args: true,
+  usage: "10 USD to IDR",
+  async execute(message, args) {
+    const normalizedArgs = args.map(arg => (arg || "").toUpperCase());
+    const isCorrectRates = RATES.includes(normalizedArgs[3]);
+    const isCorrectUsage =
+      !isNaN(args[0]) &&
+      args.length === 4 &&
+      normalizedArgs[2] === "TO" &&
+      isNaN(args[1]) &&
+      isNaN(args[3]);
+
+    if (isCorrectUsage) {
+      if (!isCorrectRates) {
+        const embed = new RichEmbed()
+          .setColor(`RANDOM`)
+          .setDescription(`Rate '${normalizedArgs[3]}' is not supported.`);
+
+        return message.channel.send(embed);
+      }
+
+      const exchangeInfo =
+        (await fetch(
+          `https://api.exchangeratesapi.io/latest?base=${normalizedArgs[1]}`
+        )
+          .then(res => res.json())
+          .catch(console.error)) || {};
+
+      if (exchangeInfo.error) {
+        const embed = new RichEmbed()
+          .setColor(`RANDOM`)
+          .setDescription(`${exchangeInfo.error}`);
+
+        return message.channel.send(embed);
+      }
+
+      const result = Math.floor(
+        Number(normalizedArgs[0]) *
+          (exchangeInfo.rates || {})[normalizedArgs[3]]
+      );
+
+      const embed = new RichEmbed()
+        .setColor(`RANDOM`)
+        .setDescription(`${args.join(" ")} = ${result}`)
+        .setFooter(`Updated: ${exchangeInfo.date}`);
+
+      return message.channel.send(embed);
+    }
+
+    message.reply(`the correct usage is: \`${prefix}cur 10 USD to IDR\``);
+  }
+};
